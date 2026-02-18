f6d2ca669c2c5aa4576f4887fc1d2d42c273d63f
Rohan-gte <rohanbiradar@gtechnohubb.com>
2026-02-18 18:21:38 +0530

    i have added user register

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") User user) {
+        userRepository.save(user);
+        return "redirect:/home";
+    }
+}
